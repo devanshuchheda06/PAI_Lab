990ed29d859c8f84d9c1c088549424914f17c723
Devanshu Chheda <devanshuchheda06@gmail.com>
2026-02-02 20:07:10 +0530

    Add files via upload
    Understanding variables, input (), print (), type() functions

---
+++ pro1.py
@@ -0,0 +1,19 @@
+# Understanding variables, input (), print (), type() functions...
+
+print ("\nData-types, Variables and input (), print () & type () functions:\n\n")
+name = input ("Enter Name: ")
+print (name)
+print (type (name))
+
+i = int (input ("\nEnter any integer: "))
+print (i)
+
+print (type (i))
+
+f = float (input ("\nEnter any floating number: "))
+print (f)
+print (type (f))
+
+b = bool (input ("\nIs 2 + 2 = 4 ? State True or False: "))
+print (b)
+print (type (b))
