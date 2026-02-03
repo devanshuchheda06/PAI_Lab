73437f6b9192a95d381983333792498cc25be99d
Devanshu Chheda <devanshuchheda06@gmail.com>
2026-02-03 17:31:19 +0530

    Add files via upload
    Understanding Addition and Subtraction operations

---
+++ pro2.py
@@ -0,0 +1,10 @@
+# Understanding Addition and Subtraction operations
+
+print ("\nAddition and Subtraction:\n\n")
+a = int (input ("Enter number a: "))
+b = int (input ("Enter number b: "))
+print ("\t-> Sum of a & b: ",(a+b))
+
+c = int (input ("\nEnter number c: "))
+d = int (input ("Enter number d: "))
+print ("\t-> Subtraction of d from c: ",(c-d))
