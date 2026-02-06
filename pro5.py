30ded3fe0bbdada7be6bdb1597148c1b345b909a
Devanshu Chheda <devanshuchheda06@gmail.com>
2026-02-06 23:57:55 +0530

    Add files via upload

---
+++ pro5.py
@@ -0,0 +1,21 @@
+# Understanding for loop
+
+print ("\nFor Loop:\n\n")
+
+print ("Loop Example 1 (without specifying initial value):")
+for i in range (6) :
+    print (i)
+
+print ("\nLoop Example 2 (with specifying initial value):")
+for j in range (1,6) :
+    print (j)
+    
+print ("\nLoop Example 3 (without specifying initial value):")
+for k in range (3) :
+    print ("Hello")
+    print (k)
+
+print ("\nLoop Example 4 (with specifying initial value):")  
+for l in range (1,3) :
+    print ("Hello")
+    print (l)
