41f7015f03adac9533f1a3c6ea01d20df55035f5
Devanshu Chheda <devanshuchheda06@gmail.com>
2026-02-10 23:12:26 +0530

    pro7.py

--- pro7.py
+++ pro7.py
@@ -1,1 +1,15 @@
-# 
+# Understanding Class and Objects in Python
+
+print ("\n=====  OOPs in Python  =====\n\n")
+
+class Student:
+    def data(self):
+        self.name = input ("Enter your name: ")
+        self.age = int (input ("Enter your age: "))
+    def display(self):
+        print ("\nEntered name:",(self.name))
+        print ("Entered age:",(self.age))
+        
+s1 = Student()
+s1.data()
+s1.display()
