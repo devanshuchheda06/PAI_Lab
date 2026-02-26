fbe8cf40e5a8d47c85dc1d759015e9cb94a6a584
Devanshu Chheda <devanshuchheda06@gmail.com>
2026-02-26 18:10:09 +0530

    Add files via upload

---
+++ pro19.py
@@ -0,0 +1,22 @@
+# Understanding Statistical Operations
+import numpy as np
+print ("\n===== Statistical Operations =====\n\n")
+a = np.array ([10, 20, 30, 40, 50])
+
+#mean
+print ("Median -> a.mean():", a.mean())
+
+#median
+print ("Median -> np.median():", np.median(a))
+
+#min
+print ("Minimum -> a.min():", a.min())
+
+#max
+print ("Maximum -> a.max():", a.max())
+
+#sum
+print ("Sum -> a.sum():", a.sum())
+
+#standard deviation
+print ("Standard Deviation -> a.std():", a.std())
